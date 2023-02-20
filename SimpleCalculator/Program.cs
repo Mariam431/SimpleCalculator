2a67b6ef6e69101385a7fc68f31f9ad8767e8a1d
mariam <mariam@MYPROBOOK>
2023-02-20 17:04:06 +0400

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator");
+            Console.WriteLine("Enter the first number:");
+            double number1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the operation (+, -, *, /):");
+            char operation = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double number2 = double.Parse(Console.ReadLine());
+
+            double result;
+
+            switch (operation)
+            {
+                case '+':
+                    result = number1 + number2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case '/':
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine("Error: division by zero");
+                    }
+                    else
+                    {
+                        result = number1 / number2;
+                        Console.WriteLine("Result: " + result);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error: invalid operation");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
